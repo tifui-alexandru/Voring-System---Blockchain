8969d6b360b3644f76a58cb59a67470891fbf2fd
tifui-alexandru <itifui00@gmail.com>
2022-04-18 17:03:50 +0300

    almost works

--- client/src/static/js/api-calls.js
+++ client/src/static/js/api-calls.js
@@ -1,27 +1,18 @@
 async function getElectionsState(web3, Ballot, accounts) {
-    // const web3 = await getWeb3();
-    // const Ballot = await getContract(web3);
-
     state = await Ballot.methods.electionState().call()
-
-    console.log("got state");
-
     return state;
 }
 
 async function callVote(voteOption, web3, Ballot, accounts) {
-    // const web3 = await getWeb3();
-    // const accounts = await web3.eth.getAccounts();
-    // const Ballot = await getContract(web3);
-
     if (voteOption == '')
         return "You must select a candidate";
 
     try {    
-        await Ballot.methods.vote(option).send({ from: accounts[0] });
-        console.log("sent vote");
+        console.log(accounts);
+        await Ballot.methods.vote(voteOption).send({ from: accounts[0] });
     } 
     catch (err) {
+        console.log(err);
         return "You either don't have the right to vote or you voted already";
     }
 
@@ -29,35 +20,68 @@ async function callVote(voteOption, web3, Ballot, accounts) {
 }
 
 async function getCandidatesNo(web3, Ballot, accounts) {
-    // const web3 = await getWeb3();
-    // const Ballot = await getContract(web3);
-
     candidatesNo = await Ballot.methods.candidatesNo().call();
     return candidatesNo;
 }
 
 async function getNthCandidate(candidateIdx, web3, Ballot, accounts) {
-    // const web3 = await getWeb3();
-    // const Ballot = await getContract(web3);
-
     try {
         candidateName = await Ballot.methods.nthCandidate(candidateIdx).call();
         return candidateName;
     }
     catch (err) {
+        console.log(err);
         return "Candidate index out of range";
     }
 }
 
 async function getNthResult(candidateIdx, web3, Ballot, accounts) {
-    // const web3 = await getWeb3();
-    // const Ballot = await getContract(web3);
-
     try {
         candidateResult = await Ballot.methods.nthNoVotes(candidateIdx).call();
         return candidateResult;
     }
     catch (err) {
+        console.log(err);
         return "Candidate index out of range";
+    }
+}
+
+async function checkAdmin(web3, Ballot, accounts) {
+    adminState = await Ballot.methods.checkOwner().call();
+
+    console.log(adminState);
+    console.log(accounts[0]);
+
+
+    return adminState;
+}
+
+async function grantAccess(address, web3, Ballot, accounts) {
+    try {
+        await Ballot.methods.grantVoter(address).call([{ from: accounts[0]}]);
+        return "Success";
+    }
+    catch (err) {
+        return "An error occured. Check if the wallet address is valid";
+    }
+}
+
+async function callStartVoting(web3, Ballot, accounts) {
+    try {
+        await Ballot.methods.startBallot().call();
+        return "Success";
+    }
+    catch (err) {
+        return "An unexpected error occured.";
+    }
+}
+
+async function callEndVoting(web3, Ballot, accounts) {
+    try {
+        await Ballot.methods.endBallot(candidateIdx).call();
+        return "Success";
+    }
+    catch (err) {
+        return "An unexpected error occured."
     }
 }
--- client/src/static/js/index.js
+++ client/src/static/js/index.js
@@ -17,7 +17,9 @@ let Ballot = undefined;
 let accounts = undefined;
 
 async function connectToContract() {
-    if (web3 == undefined) {
+    console.log(web3);
+
+    if (web3 === undefined) {
         web3 = await getWeb3();
         console.log("got web3");
     
@@ -30,23 +32,47 @@ async function connectToContract() {
 }
 
 async function vote() {
-    await connectToContract();
+    select = document.getElementById('candidates');
+    voteOption = select.options[select.selectedIndex].value;
 
-    state = await getElectionsState(web3, Ballot, accounts);
+    response = await callVote(voteOption, web3, Ballot, accounts);
+    errorCard = document.getElementById("error-card");
 
-    console.log(state);
-
-    if (state === "finished") {
-        alert("The voting has finished. You cannot vote anymore.");
-    }
-    else if (state === "not started") {
-        alert("The voting did not start yet")
+    if (response === "Success") {
+        if (errorCard !== null)
+            errorCard.parentNode.removeChild(errorCard);
+        window.location.replace(apiUrl);
     }
     else {
-        select = document.getElementById('candidates');
-        voteOption = select.options[select.selectedIndex].value;
+        if (errorCard === null) {
+            errorCard = constructErrorCard(response);
+            form = document.getElementById("voteForm");
+            form.appendChild(errorCard);
+        }
+    }
+}
 
-        response = await callVote(voteOption, web3, Ballot, accounts);
+async function getAdmin() {
+    await connectToContract();
+
+    amIAdmin = await checkAdmin(web3, Ballot, accounts);
+
+    if (amIAdmin !== true) {
+        alert("You are not admin!");
+        window.location.href = "../index.html";
+    }
+}
+
+async function grantAccess() {
+    walletAddress = document.getElementById('address').value;
+
+    state = await getElectionsState(web3, Ballot, accounts); 
+
+    if (state !== "not started") {
+        alert("You cannot grant access anymore");
+    }
+    else {
+        response = await callGrantAccess(walletAddress, web3, Ballot, accounts);
         errorCard = document.getElementById("error-card");
 
         if (response === "Success") {
@@ -64,6 +90,71 @@ async function vote() {
     }
 }
 
+async function startVoting() {
+    state = await getElectionsState(web3, Ballot, accounts); 
+
+    if (state !== "not started") {
+        alert("The election already started");
+    }
+    else {
+        response = await callStartVoting(web3, Ballot, accounts);
+        errorCard = document.getElementById("error-card");
+
+        if (response === "Success") {
+            if (errorCard !== null)
+                errorCard.parentNode.removeChild(errorCard);
+            window.location.replace(apiUrl);
+        }
+        else {
+            if (errorCard === null) {
+                errorCard = constructErrorCard(response);
+                form = document.getElementById("voteForm");
+                form.appendChild(errorCard);
+            }
+        }
+    }
+}
+
+async function endVoting() {
+    state = await getElectionsState(web3, Ballot, accounts); 
+
+    if (state !== "in progress") {
+        alert("The election is not in progress");
+    }
+    else {
+        response = await callEndVoting(web3, Ballot, accounts);
+        errorCard = document.getElementById("error-card");
+
+        if (response === "Success") {
+            if (errorCard !== null)
+                errorCard.parentNode.removeChild(errorCard);
+            window.location.replace(apiUrl);
+        }
+        else {
+            if (errorCard === null) {
+                errorCard = constructErrorCard(response);
+                form = document.getElementById("voteForm");
+                form.appendChild(errorCard);
+            }
+        }
+    }
+}
+
+async function getVotes() {
+    await connectToContract();
+
+    state = await getElectionsState(web3, Ballot, accounts); 
+
+    if (state === "finished") {
+        alert("The voting has finished. You cannot vote anymore.");
+        window.location.href = "../index.html";
+    }
+    else if (state === "not started") {
+        alert("The voting did not start yet");
+        window.location.href = "../index.html";
+    }
+}
+
 async function getResults() {
     await connectToContract();
 
@@ -76,6 +167,8 @@ async function getResults() {
     else {
         noCandidates = await getCandidatesNo(web3, Ballot, accounts);
 
+        resultContainer = document.getElementById("results-container");
+
         totalVotes = 0
         for (let i = 0; i < noCandidates; ++i)
             totalVotes += number(await getNthResult(i, web3, Ballot, accounts));
@@ -85,9 +178,33 @@ async function getResults() {
             candidateResult = number(await getNthResult(i, web3, Ballot, accounts));
     
             candidateResult = 100 * (candidateResult / totalVotes);
+
+            
+            newCandidateResult = createCandidateResult(candidateResult, candidateName);
     
-            document.getElementById('results' + i).innerText = candidateResult;
-            document.getElementById('name' + i).innerText = candidateName;
+            resultContainer.appendChild(newCandidateResult);
         }
     }
+}
+
+function createCandidateResult(result, name) {
+    // <li class="list-group-item d-flex justify-content-between align-items-center" id="element-to-clone">
+    //     <p id="name1">Candidate</p>
+    //     <span class="badge badge-primary badge-pill" id="results1">0%</span>
+    // </li>
+
+    li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between align-items-center"
+
+    p = document.createElement('p')
+    p.innerText = name
+
+    span = document.createElement('span')
+    span.innerText = result
+    span.className = "badge badge-primary badge-pill"
+
+    li.appendChild(p);
+    li.appendChild(span);
+
+    return li;
 }
